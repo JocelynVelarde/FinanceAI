490e30273ad7347dcdba91ed5d1f236ad1b0e803
Diego Rossi <dperezrossi@gmail.com>
2024-02-24 00:42:35 -0600

    prompt injection

--- api/processing.py
+++ api/processing.py
@@ -11,12 +11,42 @@ from langchain_community.llms import OpenAI
 from langchain.agents.agent_types import AgentType
 from api.auth_openai import getOpenAIkey
 
+#Hugging Face Prompt Injection
+
+from optimum.onnxruntime import ORTModelForSequenceClassification
+from transformers import AutoTokenizer, pipeline
+
+# Using https://huggingface.co/laiyer/deberta-v3-base-prompt-injection
+model_path = "laiyer/deberta-v3-base-prompt-injection"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer.model_input_names = ["input_ids", "attention_mask"]  # Hack to run the model
+model = ORTModelForSequenceClassification.from_pretrained(model_path, subfolder="onnx")
+
+classifier = pipeline(
+    "text-classification",
+    model=model,
+    tokenizer=tokenizer,
+    truncation=True,
+    max_length=512,
+)
+
+from langchain_experimental.prompt_injection_identifier import (
+    HuggingFaceInjectionIdentifier,
+)
+
+injection_identifier = HuggingFaceInjectionIdentifier(
+    model=classifier,
+)
 
 ApiKey = getOpenAIkey()
 
 def create_agent(file_path):
     # Create the agent using create_csv_agent() or a similar function
-    agent = create_csv_agent(llm=OpenAI(temperature=0), path=file_path, verbose=True, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
+    agent = create_csv_agent(
+        tools=[injection_identifier],
+        llm=OpenAI(temperature=0),
+        path=file_path,
+        verbose=True, agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
     return agent
 
 # Función para procesar el texto y crear la base de conocimientos
