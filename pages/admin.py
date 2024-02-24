4534d0fb7007f7b34f7087e8ce1839ce3db6c04e
JocelynVelarde <a01285780@itesm.mx>
2024-02-24 00:25:52 -0600

    working solution for pdf and csv assistant

--- pages/admin.py
+++ pages/admin.py
@@ -8,22 +8,6 @@ st.divider()
 
 st.write("This page is for the admin to upload the PDF and CSV files")
 
-uploaded_file = st.file_uploader('Upload your PDF Document', type='pdf')
 
-def get_pdf_file():
-    if uploaded_file is not None:
-        tfile = tempfile.NamedTemporaryFile(delete=False) 
-        tfile.write(uploaded_file.read())
-        print(tfile.name)
-        return tfile.name  # return file path
-
-
-uploaded_file_csv = st.file_uploader('Upload your CSV File', type='csv')
-
-def get_csv_file():  
-    if uploaded_file_csv is not None:
-        tfile = tempfile.NamedTemporaryFile(delete=False) 
-        tfile.write(uploaded_file_csv.read())
-        return tfile.name 
 
 
--- pages/user.py
+++ pages/user.py
@@ -8,51 +8,54 @@ import tempfile
 from pages.admin import get_pdf_file, get_csv_file
 
 
-st.write("Welcome to the user page")
+st.title("Welcome to the user page")
 st.divider()
 
-pdf = get_pdf_file()
-csv = get_csv_file()
+pdf = st.sidebar.file_uploader('Upload your PDF Document', type='pdf')
+
+st.sidebar.divider()
+
+uploaded_file = st.sidebar.file_uploader('Upload your CSV File', type='csv')
+
+query = st.text_input('Ask a question to the PDF')
 
 
-if csv is not None:
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(csv.getvalue())
-        tmp_file.flush()
-        agent = create_agent(tmp_file.name)
-
-    
-if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        # Text variable will store the pdf text
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        
-        # Create the knowledge base object
-        knowledgeBase = process_text(text)
-        
-        query = st.text_input('Ask a question to the PDF')
-        cancel_button = st.button('Cancel')
-        
-        if cancel_button:
-            st.stop()
-        
-        if query:
-            docs = knowledgeBase.similarity_search(query)
+contact_agent = st.button('Contact Agent')
+if contact_agent:
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.getvalue())
+            tmp_file.flush()
+            agent = create_agent(tmp_file.name)
             response = agent.run(query)
-            st.write(response)
+            st.write("" + response)
         
-            llm = OpenAI()
-            chain = load_qa_chain(llm, chain_type='stuff')
-                
-            with get_openai_callback() as cost:
-                response = chain.run(input_documents=docs, question=query)
-                print(cost)
+         
+cancel_button = st.button('Find solution')
+if cancel_button:
+    if pdf is not None:
+            pdf_reader = PdfReader(pdf)
+            # Text variable will store the pdf text
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
             
-            st.write(response)
+            # Create the knowledge base object
+            knowledgeBase = process_text(text)
+            
+
+            if query:
+                docs = knowledgeBase.similarity_search(query)
+            
+                llm = OpenAI()
+                chain = load_qa_chain(llm, chain_type='stuff')
+                    
+                with get_openai_callback() as cost:
+                    response = chain.run(input_documents=docs, question=query)
+                    print(cost)
+                
+                st.write(response)
 
 
 st.divider()
 st.write("Thanks for using FinanceAI!")
-
