17394a38084c87496b7282429aa79cc9085e3a7c
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-02-24 11:22:32 -0600

    Update home.py

--- home.py
+++ home.py
@@ -26,7 +26,7 @@ with user_col:
       
 st.divider()
 
-#with open('./styles/home.css') as f:
+with open('./styles/home.css') as f:
     #st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
